87a840f5508ff22a5096b940cd8647645857bb5b
keshav <paruchuri93@gmail.com>
2017-07-23 20:08:37 +0000

    comments done

--- v4/app.js
+++ v4/app.js
@@ -3,7 +3,9 @@ var express         =require("express"),
     bodyParser      =require("body-parser"),
     mongoose        =require("mongoose"),
     Campground      =require("./models/campground"),
+    Comment         =require("./models/comment"),
     seedDB          = require("./seeds");
+    
     
     
 mongoose.Promise = global.Promise;
@@ -77,9 +79,27 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
            res.render("comments/new", {campground: campground});
        } 
     });
-    
 });
 
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log("err");
+                }
+                else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+campground._id);
+                }
+            });
+        }
+    });
+});   
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Camping app started");
 });
