ce3925ce20a8471c9183d26f02f33b75b560cd4c
keshav <paruchuri93@gmail.com>
2017-07-24 18:04:30 +0000

    refactoring done

--- v6/app.js
+++ v6/app.js
@@ -7,12 +7,13 @@ var express     = require("express"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
-    seedDB      = require("./seeds");
+    seedDB      = require("./seeds")
     
-var commentsRoutes      =require("./routes/comments"),
-    campgroundsRoutes   =require("./routes/comments"),
-    indexRoutes         =require("./routes/index");
-
+//requring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/Camp", { useMongoClient: true });
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,23 +27,21 @@ app.use(require("express-session")({
     resave: false,
     saveUninitialized: false
 }));
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(indexRoutes);
-app.use(campgroundsRoutes);
-app.use(commentsRoutes);
-
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
    next();
 });
 
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The Camp Server Has Started!");
+   console.log("The YelpCamp Server Has Started!");
 });
--- v6/routes/index.js
+++ v6/routes/index.js
@@ -1,23 +1,18 @@
-var express     =require("express");
-var router      =express.Router();
-var passport    = require("passport");
-var User     =require("../models/user");
+var express = require("express");
+var router  = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
 
-
-
+//root route
 router.get("/", function(req, res){
     res.render("landing");
 });
-
-
-//  ===========
-// AUTH ROUTES
-//  ===========
 
 // show register form
 router.get("/register", function(req, res){
    res.render("register"); 
 });
+
 //handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
@@ -32,30 +27,26 @@ router.post("/register", function(req, res){
     });
 });
 
-//SHOW LOGIN PAGE
-
+//show login form
 router.get("/login", function(req, res){
-    res.render("login");
+   res.render("login"); 
 });
 
+//handling login logic
 router.post("/login", passport.authenticate("local", 
     {
-        successRedirect:  "/campgrounds",
+        successRedirect: "/campgrounds",
         failureRedirect: "/login"
     }), function(req, res){
-    
 });
 
-
-// LOGOUT ROUTE
-
+// logout route
 router.get("/logout", function(req, res){
    req.logout();
    res.redirect("/campgrounds");
 });
 
-//LOGIN CHECK
-
+//middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
